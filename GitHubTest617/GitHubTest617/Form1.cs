197ed7ffbfd9c41dd4084d88f9eb598945ba8729
Krolik555 <nathane.123c@gmail.com>
2019-07-02 14:44:41 -0500

    Just messing around with Genarics. that's all.

--- GitHubTest617/GitHubTest617/Form1.cs
+++ GitHubTest617/GitHubTest617/Form1.cs
@@ -12,9 +12,19 @@ namespace GitHubTest617
 {
     public partial class Form1 : Form
     {
+        public int num = 6;
+        public string word = "6";
         public Form1()
         {
             InitializeComponent();
+            
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = Utility.CompareValue(1,1.1f).ToString();
+            label1.Text = Utility.CompareType("cat", "fish").ToString();
+        }
+
     }
 }
---
+++ GitHubTest617/GitHubTest617/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest617
+{
+    class Utility
+    {
+        public static bool CompareValue<T01, T02>(T01 Value01, T02 Value02)
+        {
+            return Value01.Equals(Value02);
+        }
+
+        public static bool CompareType<T01, T02>(T01 t1, T02 t2)
+        {
+            return typeof(T01).Equals(typeof(T02));
+        }
+
+    }
+}
